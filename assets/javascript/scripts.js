e545ca410f0b3c6ab10ba6558a5bd0406c2d1a97
NPH82 <nich1982@gmail.com>
2017-08-08 19:53:47 -0400

    firebase added

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,9 @@
+ var config = {
+    apiKey: "AIzaSyBZAuUkeBYHmxfplYwuf-7wNHwKUFSLZcU",
+    authDomain: "turtle-project.firebaseapp.com",
+    databaseURL: "https://turtle-project.firebaseio.com",
+    projectId: "turtle-project",
+    storageBucket: "",
+    messagingSenderId: "919793437616"
+  };
+  firebase.initializeApp(config);
