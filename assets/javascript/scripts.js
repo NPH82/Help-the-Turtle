b84955a733597443d3f5f86def0ed683d7346c30
NPH82 <nich1982@gmail.com>
2017-08-08 20:59:03 -0400

    Delete scripts.js

--- assets/javascript/scripts.js
+++
@@ -1,9 +0,0 @@
- var config = {
-    apiKey: "AIzaSyBZAuUkeBYHmxfplYwuf-7wNHwKUFSLZcU",
-    authDomain: "turtle-project.firebaseapp.com",
-    databaseURL: "https://turtle-project.firebaseio.com",
-    projectId: "turtle-project",
-    storageBucket: "",
-    messagingSenderId: "919793437616"
-  };
-  firebase.initializeApp(config);
