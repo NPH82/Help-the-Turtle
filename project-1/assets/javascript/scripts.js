89e12734b3aca4f4ba0ac8a305c0547d566ad3a7
NPH82 <nich1982@gmail.com>
2017-08-10 19:59:23 -0400

    moved form.js to scripts.js

--- project-1/assets/javascript/scripts.js
+++ project-1/assets/javascript/scripts.js
@@ -69,3 +69,18 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 	infoWindow.open(map);
 }
 
+//function for button to add new turtle form information to the database
+var latitude = "latitude";
+var longitude = "longitude";
+$("#turtle-btn").on("click", function(event) {
+  event.preventDefault();
+  firebase.database().ref().push({
+    latitude: $('#latitude-input').val(),
+    longitude: $('#longitude-input').val(),
+    landmarks: $('#landmarks-input').val(),
+    name: $('#name-input').val(),
+    phonenumber: $('#phoneNumber-input').val(),
+    email: $('#email-input').val(),
+    createdAt: firebase.database.ServerValue.TIMESTAMP
+  });
+});
