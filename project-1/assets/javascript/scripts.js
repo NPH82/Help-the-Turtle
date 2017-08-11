d05b88fc9a7c85f2a4bb85b437fe45e2cf529041
NPH82 <nich1982@gmail.com>
2017-08-11 19:09:07 -0400

    long and lat variables

--- project-1/assets/javascript/scripts.js
+++ project-1/assets/javascript/scripts.js
@@ -111,44 +111,7 @@ $(document).ready(function() {
   		handleLocationError(false, infoWindow, map.getCenter());
   	}
   }
-//Google Maps API apikey: AIzaSyA4PbxtjFAOdO90WsLjM_SXs_sfUEb7OM0
 
-//Geolocation
-var map, infoWindow;
-var marker;
-
-function initMap() {
-map = new google.maps.Map(document.getElementById('map'), {
- center: {
-   lat: 41.669,
-   lng: -70.296
- },
- zoom: 8
-});
-
-infoWindow = new google.maps.InfoWindow;
-
-//Using HTML5 geolocation
-if (navigator.geolocation) {
- navigator.geolocation.getCurrentPosition(function(position) {
-   var pos = {
-     lat: position.coords.latitude,
-     lng: position.coords.longitude
-   };
-   var turtleImage = '<img id="userLocation" src="assets/images/turtle-face.jpg" alt="turtle-pic">';
-   infoWindow.setPosition(pos);
-   infoWindow.setContent(turtleImage);
-   infoWindow.open(map);
-   map.setCenter(pos);
-   map.setZoom(16);
- }, function() {
-   handleLocationError(true, infoWindow, map.getCenter());
- });
- } else {
-   //Browser doesn't suppport Geolocation
-   handleLocationError(false, infoWindow, map.getCenter());
- }
-}
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
  infoWindow.setPosition(pos);
@@ -159,8 +122,8 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 }
 
 //Submit message to database
-var latitude = "latitude";
-var longitude = "longitude";
+var latitude = position.coords.latitude;
+var longitude = position.coords.longitude;
 $("#submit").on("click", function(event) {
  event.preventDefault();
  firebase.database().ref().push({
