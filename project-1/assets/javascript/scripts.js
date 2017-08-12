e7a7431b034d647eb5f992feb106586b4385cff0
Alan Kemsley <Alan@Alans-MacBook-Pro.local>
2017-08-12 12:17:06 -0400

    Fixes Google Map and cleans up scripts.js

--- project-1/assets/javascript/scripts.js
+++ project-1/assets/javascript/scripts.js
@@ -1,3 +1,96 @@
+
+
+//////////////////////////////////////////////////////////
+//BACK END
+//////////////////////////////////////////////////////////
+
+//Define Global Variables
+
+var map, infoWindow;
+var marker;
+function initMap() {
+  infoWindow = new google.maps.InfoWindow;
+}
+
+//Google Maps API apikey: AIzaSyA4PbxtjFAOdO90WsLjM_SXs_sfUEb7OM0
+
+//Firebase Initialization
+var config = {
+  apiKey: "AIzaSyBZAuUkeBYHmxfplYwuf-7wNHwKUFSLZcU",
+  authDomain: "turtle-project.firebaseapp.com",
+  databaseURL: "https://turtle-project.firebaseio.com",
+  projectId: "turtle-project",
+  storageBucket: "",
+  messagingSenderId: "919793437616"
+};
+firebase.initializeApp(config);
+
+//Upon clicking send location
+$("#send").on("click", function(event) {
+
+  //Call initMap function
+  initMap();
+
+  //Display placeholder map
+  map = new google.maps.Map(document.getElementById('map'));
+
+  //Prevent default action
+  event.preventDefault();
+
+  //Getting geolocation information
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      infoWindow.setPosition(pos);
+      infoWindow.setContent("Turtle");
+      infoWindow.open(map);
+      map.setCenter(pos);
+      map.setZoom(16);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    //Browser doesn't suppport Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  //Send location to database
+  firebase.database().ref().push({
+    latitude: $('#latitude-input').val(),
+    longitude: $('#longitude-input').val(),
+    createdAt: firebase.database.ServerValue.TIMESTAMP
+  });
+
+  //Clears input fields
+  $("#latitude-input").val("");
+  $("#longitude-input").val("");
+
+  //Alerts user
+  Materialize.toast("Your location has been sent.", 2000);
+
+  //Posts map or error messages to DOM
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+   infoWindow.setPosition(pos);
+   infoWindow.setContent(browserHasGeolocation ?
+     'Error: We can\'t get your location.  Please refresh and accept.' :
+     'Error: Your browser doesn\'t support geolocation.');
+   infoWindow.open(map);
+  }
+
+});
+
+// On clicking submit, add:
+// landmarks: $('#landmarks-input').val(),
+// name: $('#name-input').val(),
+// phonenumber: $('#phoneNumber-input').val(),
+// email: $('#email-input').val(),
+
+
+
+
 //////////////////////////////////////////////////////////
 //FRONT END
 //////////////////////////////////////////////////////////
@@ -23,109 +116,4 @@ $(document).ready(function() {
   //Trigger modal
   $(".modal").modal();
 
-});
-
-
-
-
-//////////////////////////////////////////////////////////
-//BACK END
-//////////////////////////////////////////////////////////
-
-//Firebase Initialization
-var config = {
-  apiKey: "AIzaSyBZAuUkeBYHmxfplYwuf-7wNHwKUFSLZcU",
- authDomain: "turtle-project.firebaseapp.com",
- databaseURL: "https://turtle-project.firebaseio.com",
- projectId: "turtle-project",
- storageBucket: "",
- messagingSenderId: "919793437616"
-};
-firebase.initializeApp(config);
-
-//Google Maps API apikey: AIzaSyA4PbxtjFAOdO90WsLjM_SXs_sfUEb7OM0
-
-//Geolocation
-var map, infoWindow;
-var marker;
-
-function initMap() {
-map = new google.maps.Map(document.getElementById('map'), {
- center: {
-   lat: 41.669,
-   lng: -70.296
- },
- zoom: 8
-});
-
-infoWindow = new google.maps.InfoWindow;
-
-//Using HTML5 geolocation
-if (navigator.geolocation) {
- navigator.geolocation.getCurrentPosition(function(position) {
-   var pos = {
-     lat: position.coords.latitude,
-     lng: position.coords.longitude
-   };
-   var turtleImage = '<img id="userLocation" src="assets/images/turtle-face.jpg" alt="turtle-pic">';
-   infoWindow.setPosition(pos);
-   infoWindow.setContent(turtleImage);
-   infoWindow.open(map);
-   map.setCenter(pos);
-   map.setZoom(16);
- }, function() {
-   handleLocationError(true, infoWindow, map.getCenter());
- });
- } else {
-   //Browser doesn't suppport Geolocation
-   handleLocationError(false, infoWindow, map.getCenter());
- }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
- infoWindow.setPosition(pos);
- infoWindow.setContent(browserHasGeolocation ?
-   'Error: We can\'t get your location.  Please refresh and accept.' :
-   'Error: Your browser doesn\'t support geolocation.');
- infoWindow.open(map);
-}
-
-//Submit message to database
-var latitude = "latitude";
-var longitude = "longitude";
-$("#submit").on("click", function(event) {
-  event.preventDefault();
-   firebase.database().ref().push({
-     latitude: $('#latitude-input').val(),
-     longitude: $('#longitude-input').val(),
-     landmarks: $('#landmarks-input').val(),
-     name: $('#name-input').val(),
-     phonenumber: $('#phoneNumber-input').val(),
-     email: $('#email-input').val(),
-     createdAt: firebase.database.ServerValue.TIMESTAMP
- });
-  //Clears input fields
-  $("#latitude-input").val("");
-  $("#longitude-input").val("");
-  $("#landmarks-input").val("");
-  $("#name-input").val("");
-  $("#phoneNumber-input").val("");
-  $("#email-input").val("");
-  //Alerts user
-  Materialize.toast("Your turtle has been reported.", 2000);
-});
-
-//Send location to database
-$("#send").on("click", function(event) {
- event.preventDefault();
-  firebase.database().ref().push({
-    latitude: $('#latitude-input').val(),
-    longitude: $('#longitude-input').val(),
-    createdAt: firebase.database.ServerValue.TIMESTAMP
-  });
-  //Clears input fields
-  $("#latitude-input").val("");
-  $("#longitude-input").val("");
-  //Alerts user
-  Materialize.toast("Your location has been sent.", 2000);
 });
